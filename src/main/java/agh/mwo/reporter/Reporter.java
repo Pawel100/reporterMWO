006783a71a3d603281e59d730555181fa1d1066d
Michal Michalik <michal.michalik@gmail.com>
2018-06-23 12:31:03 +0200

    first commit

---
+++ src/main/java/agh/mwo/reporter/Reporter.java
@@ -0,0 +1,10 @@
+package agh.mwo.reporter;
+
+public class Reporter {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Reporter Project - MWO2017/2018");
+
+    }
+}
