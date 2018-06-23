de75c3b56aec8863688f57bc8e088665c8c401eb
Marcin Micek <marcin.micek@gmail.com>
2018-06-23 13:52:47 +0200

    Added Printer fields, plain title/headers printing

--- src/main/java/agh/mwo/reporter/Printer.java
+++ src/main/java/agh/mwo/reporter/Printer.java
@@ -1,5 +1,21 @@
 package agh.mwo.reporter;
 
+import java.util.ArrayList;
+
 public class Printer {
 
+	public static String title;
+	public static ArrayList<String> reportHeader;
+
+	public static void main(String[] args) {
+
+		System.out.println("Raport " + title);
+		ArrayList<String> reportHeader = new ArrayList<String>();
+		
+		for (int i = 0; i < reportHeader.size(); i++) {
+			System.out.print(reportHeader.get(i) + "    -    ");
+		}
+		
+		
+	}
 }
