20f2511f4e905e2550410f6db23e85a0769e3a67
annanke <amm3@wp.pl>
2018-06-24 10:30:50 +0200

    interface for printer and report

--- src/main/java/agh/mwo/reports/Report.java
+++ src/main/java/agh/mwo/reports/Report.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 
 import agh.mwo.reporter.Task;
 
-public abstract class Report {
+public abstract class Report implements IReport {
 
 	private String title = "";
 	private List<String> reportHeader;
@@ -14,46 +14,90 @@ public abstract class Report {
 	private LocalDate raportStartingDate;
 	private LocalDate raportEndDate;
 	
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#getTitle()
+	 */
+	@Override
 	public String getTitle() {
 		return title;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#setTitle(java.lang.String)
+	 */
+	@Override
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#getReportHeader()
+	 */
+	@Override
 	public List<String> getReportHeader() {
 		return reportHeader;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#setReportHeader(java.util.List)
+	 */
+	@Override
 	public void setReportHeader(List<String> reportHeader) {
 		this.reportHeader = reportHeader;
 	}
 	
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#getReportResults()
+	 */
+	@Override
 	public TreeMap<String, Double> getReportResults() {
 		return reportResults;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#setReportResults(java.util.TreeMap)
+	 */
+	@Override
 	public void setReportResults(TreeMap<String, Double> reportResults) {
 		this.reportResults = reportResults;
 	}
 	
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#getRaportStartingDate()
+	 */
+	@Override
 	public LocalDate getRaportStartingDate() {
 		return raportStartingDate;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#setRaportStartingDate(java.time.LocalDate)
+	 */
+	@Override
 	public void setRaportStartingDate(LocalDate raportStartingDate) {
 		this.raportStartingDate = raportStartingDate;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#getRaportEndDate()
+	 */
+	@Override
 	public LocalDate getRaportEndDate() {
 		return raportEndDate;
 	}
 
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#setRaportEndDate(java.time.LocalDate)
+	 */
+	@Override
 	public void setRaportEndDate(LocalDate raportEndDate) {
 		this.raportEndDate = raportEndDate;
 	}
 	
+	/* (non-Javadoc)
+	 * @see agh.mwo.reports.IReport#generateReport(java.util.List, java.time.LocalDate, java.time.LocalDate)
+	 */
+	@Override
 	public abstract void generateReport(List<Task> tasks, LocalDate startDate, LocalDate endDate);
 
 }
---
+++ src/main/java/agh/mwo/visualization/IPrinter.java
@@ -0,0 +1,9 @@
+package agh.mwo.visualization;
+
+import agh.mwo.reports.IReport;
+
+public interface IPrinter {
+
+	void printReport(IReport report);
+
+}
